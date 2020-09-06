92873f0d6b10c2a50b8da58b87c171a9df521e3c
Rahit <rahitkar@users.noreply.github.com>
2020-09-06 12:38:24 +0530

    changed Heading from class component to a functional compoonent

--- src/Todo/Heading.js
+++ src/Todo/Heading.js
@@ -1,58 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from './Input';
 import DeleteIcon from './DeleteIcon';
 
-class Heading extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { editable: false, isDeleteIconVisible: false };
-    this.editHeader = this.editHeader.bind(this);
-    this.updateHeading = this.updateHeading.bind(this);
-    this.showDeleteIcon = this.showDeleteIcon.bind(this);
-    this.hideDeleteIcon = this.hideDeleteIcon.bind(this);
-  }
+const Heading = (props) => {
+  const [isEditable, changeEditableState] = useState(false);
+  const [isDeleteIconVisible, changeVisibility] = useState(false);
 
-  editHeader() {
-    this.setState({ editable: true });
-  }
+  const showDeleteIcon = () => {
+    changeVisibility(true);
+  };
 
-  updateHeading(heading) {
-    this.setState({ editable: false });
-    this.props.updateHeading(heading);
-  }
+  const hideDeleteIcon = () => {
+    changeVisibility(false);
+  };
 
-  showDeleteIcon() {
-    this.setState({ isDeleteIconVisible: true });
-  }
+  const editHeader = () => {
+    changeEditableState(true);
+  };
 
-  hideDeleteIcon() {
-    this.setState({ isDeleteIconVisible: false });
-  }
+  const updateHeading = (heading) => {
+    changeEditableState(false);
+    props.updateHeading(heading);
+  };
 
-  render() {
-    const deleteIcon = this.state.isDeleteIconVisible ? (
-      <DeleteIcon delete={this.props.deleteTodo} />
-    ) : (
-      ''
+  const deleteIcon = isDeleteIconVisible ? (
+    <DeleteIcon delete={props.deleteTodo} />
+  ) : (
+    ''
+  );
+  let content = (
+    <div
+      onMouseOver={showDeleteIcon}
+      onMouseLeave={hideDeleteIcon}
+      className='task'
+    >
+      <h1 onClick={editHeader}>{props.heading}</h1>
+      {deleteIcon}
+    </div>
+  );
+
+  if (isEditable) {
+    content = (
+      <Input
+        className='edit-heading'
+        value={props.heading}
+        onKeyPress={updateHeading}
+      />
     );
-    let content = (
-      <div onMouseOver={this.showDeleteIcon} onMouseLeave={this.hideDeleteIcon} className="task">
-        <h1 onClick={this.editHeader}>{this.props.heading}</h1>
-        {deleteIcon}
-      </div>
-    );
-
-    if (this.state.editable) {
-      content = (
-        <Input
-          className='edit-heading'
-          value={this.props.heading}
-          onKeyPress={this.updateHeading}
-        />
-      );
-    }
-    return content;
   }
-}
+  return content;
+};
 
 export default Heading;
